50aa0ef587a0200369e398d5f4586bb02ccddfc4
rai8 <hytonne@outlook.com>
2019-08-27 20:15:28 +0300

    components setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Header from "./components/Header";
+import MemeGenerator from "./components/MemeGenerator";
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Header />
+      <MemeGenerator />
     </div>
   );
 }
---
+++ src/components/MemeGenerator.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class MemeGenerator extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <h1>MEME GENERATOR SECTION</h1>
+      </div>
+    );
+  }
+}
+export default MemeGenerator;
